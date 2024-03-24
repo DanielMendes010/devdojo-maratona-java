b0aa038c4bf112137357dd3ad39997a1c61f2324
DanielMendes010 <danielinacio101@gmail.com>
2024-03-24 17:18:22 -0300

    Aula 250 Padrões de Projeto pt 05 - Singleton pt 03 - Singleton with enumeration

---
+++ MaratonaJavaViradoNoJiraya/main/ZZHpadroesdeprojeto/test/AircraftSingletonEnumTest01.java
@@ -0,0 +1,16 @@
+package ZZHpadroesdeprojeto.test;
+
+import ZZHpadroesdeprojeto.dominio.AircraftSingletonEnum;
+
+public class AircraftSingletonEnumTest01 {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+    }
+
+    public static void bookSeat(String seat) {
+        System.out.println(AircraftSingletonEnum.INSTANCE.hashCode());
+        AircraftSingletonEnum instance = AircraftSingletonEnum.INSTANCE;
+        System.out.println(instance.bookSeat(seat));
+    }
+}
